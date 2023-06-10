67a6f164731f4f93ac31b113c9de367acf176636
Dharmik Dholariya <ddn092@uregina.ca>
2023-06-10 10:16:30 -0500

    added the s3 set pbject

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const app = express();
 const server = http.createServer(app);
 const path=require("path")
 const multer = require("multer");
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const fileupload = require("express-fileupload");
 // Middleware for all the requests
 
 
@@ -24,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/assets", express.static(path.join(__dirname, "/assets")));
 app.use(cors());
+app.use(fileupload());
 app.options({ origin: process.env.FRONT_END_URL}, cors());
 
 // Authentication middleware
@@ -35,20 +39,65 @@ require("./config/passport")(passport);
 require("./config/db");
 
 // upload routes 
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "assets");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
 });
 
-const upload = multer({ storage: storage });
-app.post("/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("Image has been uploaded successfully.");
+var s3 = new AWS.S3();
+
+
+
+app.post("/upload", (req, res) => {
+
+  var params = {
+    Bucket: 'look-me-up',
+    Body : req.files.file.data,
+    Key : "images/"+Date.now()+"_"+req.files.file.name
+  };
+  
+  s3.upload(params, function (err, data) {
+    if (err) {
+      res.status(200).json({
+        message: 'success!',
+      });
+    }
+    if (data) {
+     res.status(200).json({fileName:data.key})
+    }
+  });
+
+
+ 
 });
+
+app.get("/assets/:id",(req,res)=>{
+  console.log("assets",req.params.id)
+  var params = {
+    Bucket: 'look-me-up',
+    Key : req.params.id
+      };
+      s3.getObject(params, function(err, data){
+         if(err) {
+          res.status(200).json({
+            message: 'success!',
+          });
+         } else {
+             let image = new Buffer(data.Body).toString('base64');
+             image = "data:"+data.ContentType+";base64,"+image;
+             let response = {
+              "statusCode": 200,
+              "headers": {
+                  "Access-Control-Allow-Origin": "*",
+                  'Content-Type': data.ContentType
+              },
+              "body":image,
+              "isBase64Encoded": true
+          };
+      }
+      });
+  
+})
 // Routes for the api 
 app.use(require("./routes/index"));
 
